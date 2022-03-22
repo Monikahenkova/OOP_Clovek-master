767511a89ad9875e7285b80b38e76ba390fa9ef2
Monika Hénková <Henkova.Monika@stud.vassboskovice.cz>
2022-03-22 09:39:22 +0100

    Přidejte soubory projektu.

---
+++ OOP_Clovek/Clovek.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Clovek
+{
+    public class Clovek
+    {
+        // soukromé proměnné - nejsou dostupné mimo třídu Clovek
+        private int energie;
+        private int dovednost;
+
+        // vlastnosti - zveřejňují hodnoty ven z třídy
+        public string Jmeno { get; set; }
+        public string Prijmeni { get; set; }
+        public DateTime DatumNarozeni { get; set; }
+        public int Energie { get => energie; }
+        public int Zivot { get; set; }
+        public int Dovednost { get => dovednost; }
+        public int Spánek { get => energie; }
+        public int Lenost { get; set; }
+        public int Bojovat { get => dovednost; }
+
+        //konstruktor - pomocí této metody vznikne konkrétní objekt - instance třídy
+        public Clovek()
+        {
+            Jmeno = "";
+            Prijmeni = "";
+            DatumNarozeni = DateTime.Now;
+            energie = 100;
+            Zivot = 100;
+            dovednost = 0;
+            Lenost = 0;
+        }
+        
+        public void Zestarnout()
+        {
+            energie -= 10;
+            if(energie < 0)
+            {
+                energie = 0;
+            }
+            Zivot += 2;
+        }
+
+        public void ZvysitDovednost()
+        {
+            dovednost += 20;
+            energie += 10;
+        }
+         
+        public void PridatEnergii()
+        {
+            energie += 30;
+            if(energie < 100)
+            {
+                energie = 0;
+            }
+            Zivot -= 1;
+        }
+
+        public void Spát()
+        {
+            energie += 15;
+            Lenost += 25;
+            dovednost -= 5;
+        }
+
+        public void Bojovat()
+        {
+            energie -= 50;
+            Lenost -= 10;
+            Zivot -= 1;
+            dovednost += 5;
+        }
+
+
+    }
+}
---
+++ OOP_Clovek/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOP_Clovek
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Clovek karel;
+        Clovek jan;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            karel = new Clovek();
+            karel.Jmeno = "Karel";
+            karel.Prijmeni = "Cveček";
+            karel.DatumNarozeni = new DateTime(2006, 5, 24);
+            Zobraz(karel, txt1);
+
+            jan = new Clovek();
+            jan.Jmeno = "Jan";
+            Zobraz(jan, txt2);
+        }
+
+        public void Zobraz(Clovek clovek, TextBox textBox)
+        {
+            textBox.Text = clovek.Prijmeni + " " + clovek.Jmeno + "\n";
+            textBox.Text += "Datum narození: " + clovek.DatumNarozeni.ToShortDateString() + "\n";
+            textBox.Text += "Život: " + clovek.Zivot.ToString() + "\n";
+            textBox.Text += "Energie: " + clovek.Energie.ToString() + "\n";
+            textBox.Text += "Dovednost: " + clovek.Dovednost.ToString() + "\n";
+            textBox.Text += "Spánek: " + clovek.Spánek.ToString() + "\n";
+            textBox.Text += "Lenost: " + clovek.Spánek.ToString() + "\n";
+            textBox.Text += "Bojovat: " + clovek.Spánek.ToString() + "\n";
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            karel.Zestarnout();
+            jan.Zestarnout();
+            Zobraz(karel, txt1);
+            Zobraz(jan, txt2);
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            karel.ZvysitDovednost();
+            jan.ZvysitDovednost();
+            Zobraz(karel, txt1);
+            Zobraz(jan, txt2);
+
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            karel.PridatEnergii();
+            jan.PridatEnergii();
+            Zobraz(karel, txt1);
+            Zobraz(jan, txt2);
+        }
+
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            karel.Spát();
+            jan.Spát();
+            Zobraz(karel, txt1);
+            Zobraz(jan, txt2);
+        }
+
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
